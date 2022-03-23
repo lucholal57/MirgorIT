5188374867a8e1505564b7f022c7e2c033ca056b
lucholal57 <lucholal57@gmail.com>
2022-03-23 14:20:40 -0300

    STATIC_ROOT AGREGADO A BASE.PY

--- mirgorback/mirgorback/settings/base.py
+++ mirgorback/mirgorback/settings/base.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
